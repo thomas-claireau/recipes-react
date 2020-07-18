54f2b435d629252ea05e603b5f96b75e15b1a23e
Thomas Claireau <thomas.claireau@gmail.com>
2020-07-18 23:19:10 +0200

    wip ingredient controller

--- front/src/App/Ingredients/Ingredient.jsx
+++ front/src/App/Ingredients/Ingredient.jsx
@@ -1,6 +1,72 @@
-import React from 'react';
-import Site from '../Site/Site';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-export default function Ingredient() {
-	return <Site>Ingredient</Site>;
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+import './Ingredient.scss';
+import { apiFetch } from '../../utils';
+
+export default function Ingredient({ ingredient, handleAction, handleError }) {
+	const [action, setAction] = useState(null);
+	const { id, title, unit } = ingredient;
+
+	const handleSubmit = function (e) {
+		e.preventDefault();
+
+		const data = new FormData(e.target);
+
+		if (action === 'update') {
+		} else if (action === 'remove') {
+			apiFetch(`/ingredients/${id}`, { method: 'DELETE' })
+				.then(() => {
+					console.log('passe');
+					handleAction({ type: action, id });
+				})
+				.catch((e) => {
+					handleError(e.errors);
+				});
+		}
+	};
+
+	return (
+		<form className="ingredient" onSubmit={handleSubmit}>
+			<TextField
+				id="title"
+				name="title"
+				label="Titre"
+				placeholder={title}
+				defaultValue={title}
+			/>
+			<TextField id="unit" name="unit" label="Unité" placeholder={unit} defaultValue={unit} />
+			<div className="actions">
+				<Button
+					onClick={() => setAction('update')}
+					type="submit"
+					name="update"
+					fullWidth
+					variant="contained"
+					color="primary"
+				>
+					Mettre à jour
+				</Button>
+				<Button
+					onClick={() => setAction('remove')}
+					type="submit"
+					name="remove"
+					fullWidth
+					variant="contained"
+					color="secondary"
+				>
+					Supprimer
+				</Button>
+			</div>
+		</form>
+	);
 }
+
+Ingredient.propTypes = {
+	ingredient: PropTypes.object.isRequired,
+	handleAction: PropTypes.func.isRequired,
+	handleError: PropTypes.func.isRequired,
+};
--- front/src/App/Ingredients/Ingredients.jsx
+++ front/src/App/Ingredients/Ingredients.jsx
@@ -1,6 +1,107 @@
-import React from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import Site from '../Site/Site';
+import Ingredient from './Ingredient';
+
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Alert from '@material-ui/lab/Alert';
+
+import { apiFetch } from '../../utils';
+
+function reducer(state, action) {
+	switch (action.type) {
+		case 'load':
+			return action.payload;
+		case 'add':
+			return [...state, action.payload];
+		case 'update':
+			return null;
+		case 'remove':
+			return null;
+		default:
+			throw new Error(`L'action ${action.type} est inconnue`);
+	}
+}
 
 export default function Ingredients() {
-	return <Site className="ingredients">Ingredients</Site>;
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(null);
+	const [errorAdd, setErrorAdd] = useState(null);
+
+	const [ingredients, dispatch] = useReducer(reducer, null);
+
+	useEffect(() => {
+		apiFetch('/ingredients').then((ingredients) =>
+			dispatch({ type: 'load', payload: ingredients })
+		);
+	}, []);
+
+	if (ingredients === null) return 'chargement';
+
+	const handleError = function (error) {
+		if (error) setError(error);
+	};
+
+	const handleSubmit = function (e) {
+		e.preventDefault();
+
+		setLoading(true);
+		setErrorAdd(null);
+
+		const data = new FormData(e.target);
+
+		apiFetch('/ingredients', {
+			method: 'POST',
+			body: data,
+		})
+			.then((ingredient) => dispatch({ type: 'add', payload: ingredient }))
+			.catch((error) => {
+				setErrorAdd(error.errors[0].message);
+			})
+			.finally(() => {
+				setLoading(false);
+			});
+	};
+
+	const handleAction = function ({ type, id }) {
+		console.log(type);
+		console.log(id);
+	};
+
+	return (
+		<Site className="ingredients">
+			{error && <Alert severity="error">{error}</Alert>}
+			{ingredients.map((ingredient) => {
+				return (
+					<Ingredient
+						key={ingredient.id}
+						ingredient={ingredient}
+						handleAction={handleAction}
+						handleError={handleError}
+					/>
+				);
+			})}
+			{errorAdd && <Alert severity="error">{errorAdd}</Alert>}
+			<form className="ingredient" onSubmit={handleSubmit}>
+				<TextField
+					id="title"
+					name="title"
+					label="Titre"
+					placeholder="Nom de l'ingrédient"
+				/>
+				<TextField id="unit" name="unit" label="Unité" placeholder="Unité" />
+				<div className="actions">
+					<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						color="primary"
+						disabled={loading}
+					>
+						Ajouter
+					</Button>
+				</div>
+			</form>
+		</Site>
+	);
 }
